537b2875b8dfed43875756e8883f5dc3f53385d0
Jacqui Maher <jacqui.maher@nytimes.com>
2014-05-01 17:08:48 -0400

    A further wip: needs work on jee eval of constants

--- st/library/redis.go
+++ st/library/redis.go
@@ -1,10 +1,12 @@
 package library
 
 import (
+	"errors"
 	"log"
 	"time"
 
 	"github.com/fzzy/radix/redis"
+	"github.com/nytlabs/gojee"
 	"github.com/nytlabs/streamtools/st/blocks" // blocks
 	"github.com/nytlabs/streamtools/st/util"
 )
@@ -38,33 +40,96 @@ func (b *Redis) Setup() {
 // Run is the block's main loop. Here we listen on the different channels we set up.
 func (b *Redis) Run() {
 	var server string
+	var iHateThis string
 	var command string
+	var arguments []string
+	var argumentTrees []*jee.TokenTree
+	var err error
 
 	for {
 		select {
 		case ruleI := <-b.inrule:
-			server, _ = util.ParseString(ruleI, "Server")
-			command, _ = util.ParseString(ruleI, "Command")
+			server, err = util.ParseString(ruleI, "Server")
+			if err != nil {
+				b.Error(err)
+				continue
+			}
+			iHateThis = server
+			command, err = util.ParseString(ruleI, "Command")
+			if err != nil {
+				b.Error(err)
+				continue
+			}
+
+			arguments, err = util.ParseArrayString(ruleI, "Arguments")
+			if err != nil {
+				b.Error(err)
+				continue
+			}
+
+			if len(arguments) > 0 {
+				argumentTrees = make([]*jee.TokenTree, len(arguments))
+				for i, path := range arguments {
+					token, err := jee.Lexer(path)
+					if err != nil {
+						b.Error(err)
+						continue
+					}
+					tree, err := jee.Parser(token)
+					if err != nil {
+						b.Error(err)
+						continue
+					}
+					argumentTrees[i] = tree
+				}
+			}
 
 		case responseChan := <-b.queryrule:
+			log.Println(iHateThis)
 			// deal with a query request
 			responseChan <- map[string]interface{}{
-				"Server":  server,
-				"Command": command,
+				"Server":    server,
+				"Command":   command,
+				"Arguments": arguments,
 			}
 		case <-b.quit:
 			// quit the block
 			return
 		case msg := <-b.in:
-			log.Println(msg)
-			client, err := redis.DialTimeout("tcp", server, time.Duration(10)*time.Second)
+			log.Println("server:", server)
+			log.Println("iHateThis:", iHateThis)
+			client, err := redis.DialTimeout("tcp", iHateThis, time.Duration(10)*time.Second)
 			if err != nil {
 				b.Error(err)
 				continue
 			}
 			defer client.Close()
 
-			reply := client.Cmd(command)
+			var reply *redis.Reply
+
+			if argumentTrees != nil {
+				x := make([]string, len(argumentTrees))
+				for i, tree := range argumentTrees {
+					argument, err := jee.Eval(tree, msg)
+					if err != nil {
+						b.Error(err)
+						break
+					}
+					argS, ok := argument.(string)
+					if !ok {
+						b.Error(errors.New("arguments must be strings"))
+						break
+					}
+					x[i] = argS
+				}
+
+				// commands like 'KEYS *' or 'SET NUMBERS 1'
+				reply = client.Cmd(command, x)
+			} else {
+				// argument-less commands like DBSIZE
+				reply = client.Cmd(command)
+			}
+
 			if reply.Err != nil {
 				b.Error(reply.Err)
 				continue
